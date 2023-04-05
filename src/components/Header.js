d8f41a6986e7d375630ff9e18958e26d4496623d
adithtoms <adithtoms@gmail.com>
2023-04-06 00:12:47 +0530

    link to bookmark page completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import Bookmarks from './Bookmarks'
 import Nav from 'react-bootstrap/Nav';
 
 function Header() {
@@ -12,7 +11,7 @@ function Header() {
         <Navbar.Brand href='/'>Quotes</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-        <Nav.Link href='/bookmarks'><Bookmarks/></Nav.Link>
+        <Nav.Link href='/bookmarks' className=' bg-black rounded px-2 py-1'>Bookmarks</Nav.Link>
         </Navbar.Collapse>
       </Container>
       
